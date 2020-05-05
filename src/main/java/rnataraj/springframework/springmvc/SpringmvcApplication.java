392564afe4b2fbb05b99c9d03966e3000bd0483c
natrajanrajasekaran <natrajan.rajasekaran@icloud.com>
2020-05-05 14:59:41 +0800

    Move to MySQL

---
+++ src/main/java/rnataraj/springframework/springmvc/SpringmvcApplication.java
@@ -0,0 +1,19 @@
+package rnataraj.springframework.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringmvcApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = SpringApplication.run(SpringmvcApplication.class, args);
+
+        //System.out.println("Beans***");
+        //System.out.println(ctx.getBeanDefinitionCount());;
+        //System.out.println((ctx.getBeanDefinitionNames()));
+    }
+
+}
---
+++ src/test/java/rnataraj/springframework/springmvc/SpringmvcApplicationTests.java
@@ -0,0 +1,18 @@
+package rnataraj.springframework.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SpringmvcApplication.class)
+@WebAppConfiguration
+class SpringmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
